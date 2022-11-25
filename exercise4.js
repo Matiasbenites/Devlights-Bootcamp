e0ab93629c42ff0afa17bc3a257eb29fff4d8964
Matias Benites <matiasbenites665@gmail.com>
2022-11-25 10:54:54 -0300

    First commit

---
+++ exercise4.js
@@ -0,0 +1,15 @@
+// Assigning the Value of One Variable to Another - exercise 4
+
+/* Assign the contents of a to variable b.
+
+Setup
+    var a;
+    a = 7;
+    var b;
+Only change code below this line
+*/
+
+var a;
+a = 7;
+var b;
+b = a;
