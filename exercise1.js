faf767bd3e498fdb2d6a18ce17f3311f738cc98e
Matias Benites <matiasbenites665@gmail.com>
2022-11-25 10:44:28 -0300

    First commit

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+///  Comment Your JavaScript Code - exercise 1
+
+console.log("Hi everyone")//This is an in-line comment just for example.
+
+/*And this
+is a multi-line comment
+also just for example
+*/
+
+console.log("Hope to see u again!!");
---
+++ exercise2.js
@@ -0,0 +1,7 @@
+/* Declare JavaScript Variables - exercise 2
+    
+    # You should declare myName with the var keyword, 
+    ending with a semicolon
+*/ 
+
+var myName;
