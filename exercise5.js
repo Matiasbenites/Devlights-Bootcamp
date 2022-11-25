efb54d64541307c78999c5fcd6b6bf1dc0bf7e78
Matias Benites <matiasbenites665@gmail.com>
2022-11-25 12:17:25 -0300

    First commit

---
+++ exercise5.js
@@ -0,0 +1,8 @@
+/* Initializing Variables with the Assignment Operator - exercise 5
+
+
+
+    Creates a new variable called myVar and assigns it an initial value of 0.
+*/
+
+var a = 9;  
---
+++ exercise6.js
@@ -0,0 +1,12 @@
+//  Declare String Variables - exercise 6
+
+
+/*
+    Create two new string variables: myFirstName and 
+    myLastName and assign them the values of your first and last name, 
+    respectively.
+
+*/
+
+var myFirstName = "John";
+var myLastName = "Doe";
